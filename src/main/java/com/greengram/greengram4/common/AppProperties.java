c980d2dc9c8d6bd3eaa8558628b6b828d48f9827
GYEONGROK11 <gpol123@naver.com>
2024-01-09 11:57:03 +0900

    green4

---
+++ src/main/java/com/greengram/greengram4/common/AppProperties.java
@@ -0,0 +1,22 @@
+package com.greengram.greengram4.common;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@ConfigurationProperties(prefix = "app") //pre 접두사 yaml에 app끌어옴
+public class AppProperties {
+
+    private final Jwt jwt = new Jwt();//innerclass를 사용하려면 밖의 class에서 객체화를 해야함
+    @Getter
+    @Setter
+    public class Jwt{ //innerclass로 외부에서 사용을 하려고 하면 사용을 할 수 있지만, 주로 그안의 class에서 사용이 된다
+        private String secret;
+        private String headerSchemeName;
+        private String tokenType;
+        private long accessTokenExpiry;
+        private long refreshTokenExpiry;
+
+    }
+}
--- src/main/java/com/greengram/greengram4/security/MyPrincipal.java
+++ src/main/java/com/greengram/greengram4/security/MyPrincipal.java
@@ -1,12 +1,12 @@
 package com.greengram.greengram4.security;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MyPrincipal {//로그인 시 사용자의 pk값 외 다른 값을 받을때 유연하게 사용됨
     private int iuser;
 
